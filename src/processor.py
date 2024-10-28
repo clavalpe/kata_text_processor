3c09e1d6e6996d79e6aaec16306d2f729913f107
clavalpe <clavalpe@upv.es>
2024-10-28 15:48:45 +0100

    Second and third challenges completed

--- src/processor.py
+++ src/processor.py
@@ -1,4 +1,6 @@
 from abc import ABC, abstractmethod
+import math
+import re
 
 
 class Processor(ABC):
@@ -7,18 +9,40 @@ class Processor(ABC):
         ...
 
 class ProcessorImpl(Processor):
+    TOP_TEN: int = 10
+
     def analyse(self, text: str) -> str: 
         #raise NotImplementedError
-        text = self._clean_text(text)
-        frequency_occurrence = self._frequency_occurrence(text)      
-        total_words = len(frequency_occurrence)
+        filtered_text = self._remove_code_snippet(text)
+        clean_text = self._clean_text(filtered_text)
+        
+        frequency_occurrence = self._frequency_occurrence(clean_text)   
         top_ten = self._top_ten(frequency_occurrence)               
+        
+        total_words = len(clean_text)
         return f"Those are the top 10 words used: {top_ten}. The text has in total {total_words} words"
+
+    def get_time_to_read(self, text: str) -> dict:
+        clean_text = self._clean_text(text)
+        
+        number_of_words = len(clean_text)
+        words_per_minute = number_of_words/200
+        decimal_points, minutes = math.modf(words_per_minute)
+        
+        return {
+            "minutes": minutes,
+            "seconds": round(decimal_points*0.6*100)
+        }
+
+    def _remove_code_snippet(self, text):
+        return re.sub(r'` ` `.*?` ` `', '', text)
 
     def _clean_text(self, text: str) -> str:
         text = text.replace(",", "")
         text = text.replace(".", "")
+        text = text.replace(":", "")
         text = text.lower()
+        text = text.strip()
         text = text.split(" ")
         return text
     
@@ -28,19 +52,23 @@ class ProcessorImpl(Processor):
         for word in text:
             if word in frequency_occurrence:
                 frequency_occurrence[word] += 1
-                continue                
+                continue
+
             frequency_occurrence[word] = 1
 
         return frequency_occurrence
     
     def _top_ten(self, frequency_occurrence: dict) -> str:
-        frequency_occurrence_ordered = sorted(frequency_occurrence.items(), key=lambda x: x[1], reverse=True)
+        frequency_occurrence_ordered = self._sort_by_occurrence_descendent(frequency_occurrence)
 
         top_ten = ""
         current_word_ranking = 0
         for word, _ in frequency_occurrence_ordered:
             current_word_ranking+=1
-            if current_word_ranking <= 10:
+            if current_word_ranking <= self.TOP_TEN:
                 top_ten += " " + str(current_word_ranking)+". "+word   
 
         return top_ten
+
+    def _sort_by_occurrence_descendent(self, frequency_occurrence):
+        return sorted(frequency_occurrence.items(), key=lambda x: x[1], reverse=True)    
--- src/test/test_processor.py
+++ src/test/test_processor.py
@@ -7,13 +7,25 @@ class TestProcessor:
 
         actual_analysis = ProcessorImpl().analyse(sentence_to_proccess)
 
-        expected_analysis = "Those are the top 10 words used:  1. this 2. you 3. hello 4. is 5. an 6. example 7. for 8. to 9. practice 10. should. The text has in total 19 words"
+        expected_analysis = "Those are the top 10 words used:  1. this 2. you 3. hello 4. is 5. an 6. example 7. for 8. to 9. practice 10. should. The text has in total 21 words"
         assert actual_analysis == expected_analysis
 
-    # def test_it_returns_the_most_common_word(self):
-    #     sentence_to_proccess = "Hello, this is an example for you to practice. You should grab this text and make it as your test case."
 
-    #     actual_analysis = ProcessorImpl().analyse(sentence_to_proccess)
+    def test_it_return_how_much_time_takes_the_text_to_be_read(self):
+        sentence_to_proccess = "Hello, this is an example for you to practice. You should grab this text and make it as your test case."
 
-    #     expected_analysis = "you"
-    #     assert actual_analysis == expected_analysis
+        actual_analysis = ProcessorImpl().get_time_to_read(sentence_to_proccess)
+
+        expected_analysis = {
+            "minutes": 0,
+            "seconds": 6
+        }
+        assert actual_analysis == expected_analysis
+    
+    def test_it_analyse_the_text_ignoring_code_snippets(self):
+        sentence_to_proccess = "Hello, this is an example for you to practice. You should grab this text and make it as your test case: ` ` `javascript if (true) {console.log('should should should')}` ` `"
+
+        actual_analysis = ProcessorImpl().analyse(sentence_to_proccess)
+
+        expected_analysis = "Those are the top 10 words used:  1. this 2. you 3. hello 4. is 5. an 6. example 7. for 8. to 9. practice 10. should. The text has in total 21 words"
+        assert actual_analysis == expected_analysis
